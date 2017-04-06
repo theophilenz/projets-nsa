b189eb186bb7ef97fe2e0f5f1e4262cf28244c5e
Theophile Nzungize <theophile.nz@gmail.com>
2017-04-06 00:30:27 -0400

    Page d'accueil presque complète

--- public/appli/page-accueil/controlleur-accueil.js
+++ public/appli/page-accueil/controlleur-accueil.js
@@ -2,5 +2,6 @@ angular.module("projetsnsa").controller("controlleurAccueil", controlleurAccueil
 
 function controlleurAccueil() {
     var vm = this;
-    vm.titre = "Bienvenue";
+    vm.titre = "Le spécialiste de la rénovation résidentielle";
+    vm.lead = "Profitez du savoir-faire de Projets NSA et accueillez chez-vous une équipe de professionnels qui pourra se charger de vos différents travaux de rénovation.";
 }
